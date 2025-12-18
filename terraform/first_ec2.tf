d28237ecd0ef844fbe9085c8bf4602bf64334e29
Shamit Mittal <shamit1612@gmail.com>
2025-12-18 11:19:12 +0000

    updated tag

--- terraform/first_ec2.tf
+++ terraform/first_ec2.tf
@@ -1,10 +1,14 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = "ap-southeast-2"
   access_key = "AKIAZ46F6WUDG5FX7AYC"
   secret_key = "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"
 }
 
-resource "aws_instance" "first-aws-ec2" { //first-aws-ec2
+resource "aws_instance" "first-aws-ec2" {
   ami           = "ami-0b3c832b6b7289e44"
   instance_type = "t3.micro"
+
+  tags = {
+    Name ="first-aws-ec2"
+  }
 }
