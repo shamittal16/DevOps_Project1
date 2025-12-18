c015a916d615b794828a7a20e70badb89fe3185a
Shamit Mittal <shamit1612@gmail.com>
2025-12-18 10:42:57 +0000

    tf-config

---
+++ terraform/first_ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAZ46F6WUDG5FX7AYC"
+  secret_key = "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"
+}
+
+resource "aws_instance" "first-aws-ec2" { //first-aws-ec2
+  ami           = "ami-0b3c832b6b7289e44"
+  instance_type = "t3.micro"
+}
