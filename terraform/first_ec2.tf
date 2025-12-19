1b09a9743425c2e94e6f6e5f3a9ae2b39d5564ec
Shamit Mittal <shamit1612@gmail.com>
2025-12-19 18:48:31 +0000

    replicating issue #19

--- terraform/first_ec2.tf
+++ terraform/first_ec2.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region     = var.aws_region
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "first-aws-ec2" {
-  ami           = "ami-0b3c832b6b7289e44"
+  ami           = "ami-068c0051b15cdb816"
   instance_type = "t3.micro"
 
   tags = {
-    Name ="first-ec2-instance"
+    Name = "instance-1"
   }
 }
