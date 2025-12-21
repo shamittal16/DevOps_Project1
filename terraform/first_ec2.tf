4c7fa363f3955edb8bd3fc81c070bafef58a8b2e
Shamit Mittal <shamit1612@gmail.com>
2025-12-21 15:13:40 +0000

    inbound/outbound creation

--- terraform/first_ec2.tf
+++ terraform/first_ec2.tf
@@ -10,11 +10,28 @@ resource "aws_instance" "first-aws-ec2" {
     Name = "instance-1"
   }
 }
-resource "aws_security_group" "mysg" {
-  name = "terraform-sg"
+
+resource "aws_security_group" "tf-sg" {
+  name = "terraform-firewall-sg"
   description = "Security group managed by tf"
 
   tags = {
-    Name = "terraform-sg"
+    Name = "terraform-firewall-sg"  
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow-http-ingress" {
+  security_group_id = aws_security_group.tf-sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow-all" {
+  security_group_id = aws_security_group.tf-sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
 }
